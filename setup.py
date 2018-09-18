a7633e585b622ab7a56ad8ed547135d98d19a0ec
lbusoni <lbusoni@gmail.com>
2018-09-18 16:21:28 +0200

    setup.py more automatic, upload to pypi and github tag

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
+import os
+import sys
+from shutil import rmtree
+
 from setuptools import setup, Command
 
+NAME = 'plico'
+DESCRIPTION = 'Python Laboratory Instrumentation COntrol'
+URL = 'https://github.com/lbusoni/plico'
+EMAIL = 'lorenzo.busoni@inaf.it'
+AUTHOR = 'Lorenzo Busoni'
+LICENSE= 'MIT'
+KEYWORDS= 'laboratory, instrumentation control''laboratory, instrumentation control'
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
 
 
 
@@ -32,28 +50,29 @@ class UploadCommand(Command):
         os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
 
         self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
+        #os.system('twine upload dist/*')
 
         self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-        
+        #os.system('git tag v{0}'.format(about['__version__']))
+        #os.system('git push --tags')
+
         sys.exit()
 
-setup(name='plico',
-      description='Python Laboratory Instrumentation COntrol',
-      version='0.14',
+
+setup(name=NAME,
+      description=DESCRIPTION,
+      version=about['__version__'],
       classifiers=['Development Status :: 4 - Beta',
                    'Operating System :: POSIX :: Linux',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
                    ],
       long_description=open('README.md').read(),
-      url='',
-      author_email='lbusoni@gmail.com',
-      author='Lorenzo Busoni',
-      license='',
-      keywords='laboratory, instrumentation control',
+      url=URL,
+      author_email=EMAIL,
+      author=AUTHOR,
+      license=LICENSE,
+      keywords=KEYWORDS,
       packages=['plico',
                 'plico.client',
                 'plico.rpc',
@@ -71,4 +90,5 @@ setup(name='plico',
                         ],
       include_package_data=True,
       test_suite='test',
+      cmdclass={'upload': UploadCommand, },
       )
