0ea5328c217062a44da98e2e5a2f51e8f056f60b
Lorenzo Busoni <lbusoni@gmail.com>
2022-01-16 01:54:19 +0100

    default log_level for server is warning

--- plico/rpc/zmq_remote_procedure_call.py
+++ plico/rpc/zmq_remote_procedure_call.py
@@ -9,13 +9,10 @@ from plico.utils.constants import Constants
 from plico.rpc.abstract_remote_procedure_call import \
     AbstractRemoteProcedureCall
 
-
-
-
 if sys.version_info[0] >= 3:
-    pickle_options={'encoding': 'latin1'}
+    pickle_options = {'encoding': 'latin1'}
 else:
-    pickle_options={}
+    pickle_options = {}
 
 
 class ZmqRpcTimeoutError(Exception):
@@ -25,40 +22,35 @@ class ZmqRpcTimeoutError(Exception):
 class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
 
     def __init__(self, timeModule=time):
-        self._context= zmq.Context()
-        self._logger= Logger.of("ZmqRPC")
-        self._timeMod= timeModule
-
+        self._context = zmq.Context()
+        self._logger = Logger.of("ZmqRPC")
+        self._timeMod = timeModule
 
     @override
     @returnsNone
     def publishPickable(self, socket, pickableObject):
-        resPickled= pickle.dumps(pickableObject, Constants.PICKLE_PROTOCOL)
+        resPickled = pickle.dumps(pickableObject, Constants.PICKLE_PROTOCOL)
         self._logger.debug("sending %s (pickle size: %d)" %
                            (str(pickableObject), len(resPickled)))
         socket.send(resPickled, zmq.NOBLOCK)
 
-
     @override
     def receivePickable(self, socket, timeoutInSec=10):
-        toBeReturned= self.receiveWithTimeout(socket, timeoutInSec)
-        retObj= pickle.loads(toBeReturned, **pickle_options)
+        toBeReturned = self.receiveWithTimeout(socket, timeoutInSec)
+        retObj = pickle.loads(toBeReturned, **pickle_options)
         if isinstance(retObj, Exception):
             raise retObj
         else:
             return retObj
-
 
     @override
     @returnsNone
     def sendCameraFrame(self, socket, frame):
         self.publishPickable(socket, frame)
 
-
     @override
     def recvCameraFrame(self, socket, timeoutInSec=10):
         return self.receivePickable(socket, timeoutInSec)
-
 
     def publisherSocket(self,
                         port,
@@ -79,14 +71,12 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
             else:
                 socket.bind(self.tcpAddress(host, port))
         except Exception as e:
-            newMsg= str("%s %s:%d" % (str(e), host, port))
+            newMsg = str("%s %s:%d" % (str(e), host, port))
             raise (type(e))(newMsg)
         return socket
 
-
     def tcpAddress(self, host, port):
         return "tcp://%s:%d" % (host, port)
-
 
     def subscriberSocket(self, host, port, filt=b'', conflate=False):
         '''
@@ -99,7 +89,6 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
         socket.connect(self.tcpAddress(host, port))
         socket.setsockopt(zmq.SUBSCRIBE, filt)
         return socket
-
 
     def xpubsubSockets(self, hostSub, portSub, hostPub, portPub):
         '''
@@ -117,7 +106,6 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
 
         return frontendSocket, backendSocket
 
-
     @cacheResult
     def replySocket(self, port, host='*'):
         '''
@@ -127,10 +115,9 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
             socket = self._context.socket(zmq.REP)
             socket.bind(self.tcpAddress(host, port))
         except Exception as e:
-            newMsg= str("%s %s:%d" % (str(e), host, port))
+            newMsg = str("%s %s:%d" % (str(e), host, port))
             raise (type(e))(newMsg)
         return socket
-
 
     @cacheResult
     def requestSocket(self, host, port):
@@ -141,17 +128,15 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
         socket.connect(self.tcpAddress(host, port))
         return socket
 
-
     def _discardPendingAnswers(self, socket):
         try:
-            buf= socket.recv(zmq.NOBLOCK)
+            buf = socket.recv(zmq.NOBLOCK)
             self._logger.debug("got pending buffer %d bytes (%s)" %
                                (len(buf), buf))
         except zmq.ZMQError:
             return
         except Exception as e:
             self._logger.debug("got Exception %s" % str(e))
-
 
     def _hasSendMultiPartSucceded(self, socket, multiPartMsg):
         try:
@@ -163,7 +148,6 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
                                (multiPartMsg[0].decode(), str(e)))
             return False
 
-
     def _sendMultiPartWithBarrierTimeout(self, socket,
                                          multiPartMsg, timeoutSec):
         try:
@@ -172,8 +156,6 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
                                          socket, multiPartMsg))
         except BarrierTimeout as e:
             raise ZmqRpcTimeoutError(str(e))
-
-
 
     def sendRequest(self, socket, cmd, args=(), timeout=10):
         '''
@@ -184,21 +166,20 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
         Reply is a pickled object
         '''
         self._logger.debug("sending request %s %s" % (cmd, args))
-        t0= time.time()
+        t0 = time.time()
         self._sendMultiPartWithBarrierTimeout(
             socket,
             [cmd.encode(),
              pickle.dumps(args, Constants.PICKLE_PROTOCOL)],
             timeout)
-        toBeReturned= self.receiveWithTimeout(socket, timeout)
-        retObj= pickle.loads(toBeReturned, **pickle_options)
+        toBeReturned = self.receiveWithTimeout(socket, timeout)
+        retObj = pickle.loads(toBeReturned, **pickle_options)
         self._logger.debug("%s received %s in %.3fs" % (
             cmd, str(retObj), time.time() - t0))
         if isinstance(retObj, Exception):
             raise retObj
         else:
             return retObj
-
 
     def receiveWithTimeout(self, socket, timeoutInSeconds=1):
         poller = zmq.Poller()
@@ -209,7 +190,6 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
         else:
             self._logger.debug("raising ZmqRpcTimeoutError")
             raise ZmqRpcTimeoutError()
-
 
     @override
     def handleRequest(self, obj, socket, multi=False):
@@ -230,11 +210,11 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
                 self._logger.debug("received request %s %s" %
                                    (method, str(args)))
                 try:
-                    res= getattr(obj, method).__call__(*args)
+                    res = getattr(obj, method).__call__(*args)
                 except Exception as e:
                     self._logger.notice('Request %s %s failed. Caught %s %s' %
                                         (method, str(args), type(e), str(e)))
-                    res= e
+                    res = e
                 self._sendAnswer(socket, res)
             except zmq.ZMQError:
                 return
@@ -243,9 +223,8 @@ class ZmqRemoteProcedureCall(AbstractRemoteProcedureCall):
             if not multi:
                 break
 
-
     def _sendAnswer(self, socket, answer):
-        resPickled= pickle.dumps(answer, Constants.PICKLE_PROTOCOL)
+        resPickled = pickle.dumps(answer, Constants.PICKLE_PROTOCOL)
         self._logger.debug("answering %s (pickle size: %d)" %
                            (str(answer), len(resPickled)))
         socket.send(resPickled, zmq.NOBLOCK)
--- plico/utils/base_runner.py
+++ plico/utils/base_runner.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-import logging
 import logging.handlers
 import os
 import sys
@@ -10,59 +9,53 @@ from plico.rpc.zmq_remote_procedure_call import ZmqRemoteProcedureCall
 from plico.rpc.sockets import Sockets
 from plico.rpc.zmq_ports import ZmqPorts
 
-__version__= "$Id: base_runner.py 54 2018-04-30 21:00:26Z lbusoni $"
-
 
 class BaseRunner(object):
 
     def __init__(self):
-        INITIALIZED_LATER= None
-        self._configFilePath= INITIALIZED_LATER
-        self._rpc= INITIALIZED_LATER
-        self._configuration= INITIALIZED_LATER
-        self._sockets= INITIALIZED_LATER
-        self._argv= INITIALIZED_LATER
-
+        INITIALIZED_LATER = None
+        self._configFilePath = INITIALIZED_LATER
+        self._rpc = INITIALIZED_LATER
+        self._configuration = INITIALIZED_LATER
+        self._sockets = INITIALIZED_LATER
+        self._argv = INITIALIZED_LATER
 
     def _logRunning(self):
         self._logger.notice(self.RUNNING_MESSAGE)
         sys.stdout.flush()
 
-
     def _createFoldersIfMissing(self, filename):
         if not os.path.isdir(os.path.dirname(filename)):
             os.makedirs(os.path.dirname(filename))
 
-
     def _determineLoggingLevel(self):
         try:
-            levelAsStr= self._configuration.logLevel(
+            levelAsStr = self._configuration.logLevel(
                 self._configurationSection)
         except KeyError:
-            levelAsStr= 'debug'
+            levelAsStr = 'debug'
         if levelAsStr == 'fatal':
-            loggingLevel= logging.CRITICAL
+            loggingLevel = logging.CRITICAL
         elif levelAsStr == 'error':
-            loggingLevel= logging.ERROR
+            loggingLevel = logging.ERROR
         elif levelAsStr == 'warning':
-            loggingLevel= logging.WARNING
+            loggingLevel = logging.WARNING
         elif levelAsStr == 'info':
-            loggingLevel= logging.INFO
+            loggingLevel = logging.INFO
         elif levelAsStr == 'debug':
-            loggingLevel= logging.DEBUG
+            loggingLevel = logging.DEBUG
         else:
-            loggingLevel= logging.DEBUG
+            loggingLevel = logging.WARNING
         return loggingLevel
 
-
     def _setUpLogging(self, logFilePath):
-        loggingLevel= self._determineLoggingLevel()
+        loggingLevel = self._determineLoggingLevel()
         print(('Setting up logging in file %s level %s' % (
             logFilePath, loggingLevel)))
         self._createFoldersIfMissing(logFilePath)
-        FORMAT='%(asctime)s %(levelname)s %(message)s'
+        FORMAT = '%(asctime)s %(levelname)s %(message)s'
         f = logging.Formatter(fmt=FORMAT)
-        rotHandler= logging.handlers.RotatingFileHandler(
+        rotHandler = logging.handlers.RotatingFileHandler(
             logFilePath, encoding='utf8',
             maxBytes=10000000, backupCount=10)
         handlers = [rotHandler]
@@ -74,22 +67,19 @@ class BaseRunner(object):
             root_logger.addHandler(h)
         rotHandler.doRollover()
 
-
     def _createConfiguration(self):
-        self._configuration= Configuration()
+        self._configuration = Configuration()
         self._configuration.load(self._configFilePath)
 
-
     def _createZmqBasedRPC(self):
-        zmqPorts= ZmqPorts.fromConfiguration(self._configuration,
-                                             self._configurationSection)
-        self._rpc= ZmqRemoteProcedureCall()
-        self._sockets= Sockets(zmqPorts, self._rpc)
-
+        zmqPorts = ZmqPorts.fromConfiguration(self._configuration,
+                                              self._configurationSection)
+        self._rpc = ZmqRemoteProcedureCall()
+        self._sockets = Sockets(zmqPorts, self._rpc)
 
     def _checkCommandLine(self, argv):
         if 3 > len(argv):
-            programName= argv[0]
+            programName = argv[0]
             print("Usage:", file=sys.stderr)
             print("\t%s <configuration_file> <configuration_section>" %
                   programName, file=sys.stderr)
@@ -100,20 +90,17 @@ class BaseRunner(object):
             sys.exit(os.EX_USAGE)
         print('argv: %s' % str(argv))
 
-
     def _parseCommandLine(self, argv):
         self._checkCommandLine(argv)
-        self._configFilePath= argv[1]
-        self._configurationSection= argv[2]
-
+        self._configFilePath = argv[1]
+        self._configurationSection = argv[2]
 
     def _logFilePath(self):
         return os.path.join(self._configuration.loggingDir(),
                             '%s.log' % self._configurationSection)
 
-
     def start(self, argv):
-        self._argv= argv
+        self._argv = argv
         self._parseCommandLine(argv)
         self._createConfiguration()
         self._setUpLogging(self._logFilePath())
@@ -121,14 +108,11 @@ class BaseRunner(object):
         self._registrerHandlers()
         return self.run()
 
-
     def run(self):
         pass
 
-
     def getConfigFilePath(self):
         return self._configFilePath
-
 
     @property
     def name(self):
@@ -136,46 +120,36 @@ class BaseRunner(object):
             self._configurationSection,
             'name')
 
-
     @property
     def configuration(self):
         return self._configuration
 
-
     def getConfigurationSection(self):
         return self._configurationSection
-
 
     def rpc(self):
         return self._rpc
 
-
     def sockets(self):
         return self._sockets
-
 
     def argv(self):
         return self._argv
 
-
     def terminate(self, signal, frame):
         pass
-
 
     def _registerHandlerForSigInt(self):
         signal.signal(signal.SIGINT, self.terminate)
 
-
     def _registerHandlerForSigTerm(self):
         signal.signal(signal.SIGTERM, self.terminate)
-
 
     def _registerHandlerForSigKill(self):
         try:
             signal.signal(signal.SIGKILL, self.terminate)
         except Exception:
             pass
-
 
     def _registrerHandlers(self):
         self._registerHandlerForSigInt()
