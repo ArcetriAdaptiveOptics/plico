16d21ba5f294dc268c53aaffd88ed528335b1ec9
Lorenzo Busoni <lbusoni@gmail.com>
2024-05-12 21:19:03 +0000

    docs updated

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-master_doc = 'source/index'
+master_doc = 'index'
 
 # General substitutions.
 
